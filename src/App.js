3a29d307a7aef7079612b4da59e071626e2bb64b
jini815 <jinijoa0@gmail.com>
2024-11-22 13:16:20 +0900

    ボタンにする

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Button } from '@mui/material';
 import './App.css';
 import HomeIcon from '@mui/icons-material/Home';
 
@@ -5,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HomeIcon />
+        <Button>
+          <HomeIcon />
+        </Button>
       </header>
     </div>
   );
