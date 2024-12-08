e1a6e286d33d826cd67f23cb8b245151f3a0bc1f
und-jini <choiyongjin@undershaft.net>
2024-12-08 12:04:06 +0900

    add BrowserRouter, Routs, Route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Sidebar from "./Sidebar";
+import Main from "./pages/Main";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Email from "./pages/Email";
+import Access from "./pages/Access";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Sidebar />
-        <div className="content"></div>
+        <Main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/email" element={<Email />} />
+            <Route path="/access" element={<Access />} />
+          </Routes>
+        </Main>
       </div>
     </BrowserRouter>
   );
