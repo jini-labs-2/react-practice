040e99a8ed163fff73f6e2e49b264acc75d64e83
und-jini <choiyongjin@undershaft.net>
2024-12-10 08:40:27 +0900

    add utility pages.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Email from "./pages/Email";
 import Access from "./pages/Access";
+import Cidr from "./pages/contents/Cidr";
+import Uuidv1 from "./pages/contents/Uuidv1";
+import Uuidv3 from "./pages/contents/Uuidv3";
+import Uuidv4 from "./pages/contents/Uuidv4";
+import Uuidv5 from "./pages/contents/Uuidv5";
+import Cuid from "./pages/contents/Cuid";
+import Ulid from "./pages/contents/Ulid";
+import Lid from "./pages/contents/Lid";
+import Nanoid from "./pages/contents/Nanoid";
+import Shortid from "./pages/contents/Shortid";
 
 function App() {
   return (
@@ -18,6 +28,16 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/email" element={<Email />} />
             <Route path="/access" element={<Access />} />
+            <Route path="/Cidr" element={<Cidr />} />
+            <Route path="/ids/uuidv1" element={<Uuidv1 />} />
+            <Route path="/ids/uuidv3" element={<Uuidv3 />} />
+            <Route path="/ids/uuidv4" element={<Uuidv4 />} />
+            <Route path="/ids/uuidv5" element={<Uuidv5 />} />
+            <Route path="/ids/cuid" element={<Cuid />} />
+            <Route path="/ids/lid" element={<Lid />} />
+            <Route path="/ids/ulid" element={<Ulid />} />
+            <Route path="/ids/nanoid" element={<Nanoid />} />
+            <Route path="/ids/shortid" element={<Shortid />} />
           </Routes>
         </Main>
       </div>
--- src/constants/Menu.js
+++ src/constants/Menu.js
@@ -1,10 +1,19 @@
 import HomeIcon from '@mui/icons-material/Home';
 import Email from '@mui/icons-material/Email';
 import AnalyticsIcon from '@mui/icons-material/Analytics';
+import Info from '@mui/icons-material/Info';
+import { CenterFocusStrongOutlined, Domain, FormatItalicOutlined, FormatItalicRounded, FormatItalicSharp, FormatItalicTwoTone, Gamepad, LibraryBooks, LibraryBooksOutlined, LibraryBooksRounded } from '@mui/icons-material';
 
 export const SidebarMenus = [
   { name: "home", icon: <HomeIcon />, link: "/" },
-  { name: "about", icon: <AnalyticsIcon />, link: "/about" },
-  { name: "email", icon: <Email />, link: "/email" },
-  { name: "access", icon: <Email />, link: "/access" },
+  { name: "CIDR", icon: <Domain />, link: "/Cidr" },
+  { name: "UUIDv1", icon: <FormatItalicOutlined />, link: "/ids/uuidv1" },
+  { name: "UUIDv3", icon: <FormatItalicRounded />, link: "/ids/uuidv3" },
+  { name: "UUIDv4", icon: <FormatItalicSharp />, link: "/ids/uuidv4" },
+  { name: "UUIDv5", icon: <FormatItalicTwoTone />, link: "/ids/uuidv5" },
+  { name: "LID", icon: <LibraryBooks />, link: "/ids/lid" },
+  { name: "ULID", icon: <LibraryBooksOutlined />, link: "/ids/ulid" },
+  { name: "CUID", icon: <LibraryBooksRounded />, link: "/ids/cuid" },
+  { name: "NanoID", icon: <CenterFocusStrongOutlined />, link: "/ids/nanoid" },
+  { name: "ShortId", icon: <Gamepad />, link: "/ids/shortid" },
 ]
