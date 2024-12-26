78e7c515996e58905ab9759927e797167b12bb26
und-jini <choiyongjin@undershaft.net>
2024-12-27 01:53:30 +0900

    use reducer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import { useState } from 'react';
-import './App.css';
 import { useReducer } from 'react';
+import './App.css';
 
 function App() {
-  function countReducer() {
+  function countReducer(current, action) {
+    console.log('--s0000--', current, action)
+    switch(action.type) {
+      case 'UP':
+        return current + action.scale;
+      case 'DOWN':
+        return current - action.scale;
+      default:
+        break;
+    }
   }
 
-  const [count, setCount] = useReducer(countReducer, 0)
+  const [count, countDispatch] = useReducer(countReducer, 0)
+  function upFn() {
+    countDispatch({type:'UP', scale: 1})
+  }
+  function downFn() {
+    countDispatch({type:'DOWN', scale: 1})
+  }
+
   return (
     <div className="App">
       <h1>Current count : {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Count UP</button>
-      <button onClick={() => setCount(count - 1)}>Count DOWN</button>
+      <button onClick={upFn}>Count UP</button>
+      <button onClick={downFn}>Count DOWN</button>
     </div>
   );
 }
