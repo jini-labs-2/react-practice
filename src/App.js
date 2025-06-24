e8919551787c7e74e02b59b849586debe4c8f842
Your Name <you@example.com>
2025-06-24 23:37:21 +0900

    この状態がよい

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
-import InputForm from './components/InputForm';
 import useInput from './hooks/useInput';
 
 function App() {
   const [inputValue, handleChange] = useInput('別名');
+  const [inputValue2, handleChange2] = useInput('アドレス');
+
+  const handleSubmit = () => {
+    alert('Input value is "', inputValue, '"')
+  }
 
   return (
     <div className="App">
       <h1>useInput</h1>
-      <input value={inputValue} onChange={handleChange} />
-      <button> Ok </button>
-
-      <hr style={{margin: "10px, 0"}}/>
-      <h1>useForm</h1>
-      <InputForm />
-
+      <input value={inputValue} onChange={handleChange} /> <br/>
+      <input value={inputValue2} onChange={handleChange2} /> <br/>
+      <button onClick={handleSubmit}> Ok </button>
     </div>
   );
 }
