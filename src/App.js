0072db2f5808f45219d148165fbc30c83f8335a0
Your Name <you@example.com>
2025-06-24 23:43:29 +0900

    useInput hook & displayMessage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import './App.css';
 import useInput from './hooks/useInput';
 
-function App() {
-  const [inputValue, handleChange] = useInput('別名');
-  const [inputValue2, handleChange2] = useInput('アドレス');
+function displayMessage(message) {
+  alert(message)
+}
 
-  const handleSubmit = () => {
-    alert('Input value is "', inputValue, '"')
-  }
+function App() {
+  const [inputValue, handleChange, handleSubmit] = useInput('別名', displayMessage);
 
   return (
     <div className="App">
       <h1>useInput</h1>
       <input value={inputValue} onChange={handleChange} /> <br/>
-      <input value={inputValue2} onChange={handleChange2} /> <br/>
       <button onClick={handleSubmit}> Ok </button>
     </div>
   );
--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,10 +1,15 @@
 import { useState } from "react";
 
-export default function useInput(initValue) {
+export default function useInput(initValue, submitAction) {
   const [inputValue, setInputValue] = useState(initValue);
   const handleChange = (e) => {
     setInputValue(e.target.value);
   }
 
-  return [inputValue, handleChange];
+  const handleSubmit = () => {
+    setInputValue("");
+    submitAction(inputValue)
+  }
+
+  return [inputValue, handleChange, handleSubmit];
 }
