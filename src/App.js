78200a853daa77102f324d76d8a45b96b9219c07
Your Name <you@example.com>
2025-06-28 23:21:07 +0900

    useState vs useRef(counter) vs variabe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  // count値の変化でレンダーリングされる。
+  const [count, setCount] = useState(0);
+  // countRef値の変化のみではレンダーリングされ無い
+  const countRef = useRef(0);
+  // レンダーリングたびに初期化される。
+  let countVal = 0;
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  }
+
+  const incrementCountRef = () => {
+    countRef.current = countRef.current + 1;
+  }
+
+  const incrementCountVal = () => {
+    countVal = countVal + 1;
+  }
+
+  console.log('rendering')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>steat : {count}</p>
+      <p>Ref: {countRef.current}</p>
+      <p>Val: {countVal}</p>
+      <button onClick={incrementCount}> count up </button>
+      <button onClick={incrementCountRef}> count Ref up </button>
+      <button onClick={incrementCountVal}> count val up </button>
     </div>
   );
 }
