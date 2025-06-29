03a956a1f91caab540ca9e24bcfeed609f72ad8e
Your Name <you@example.com>
2025-06-29 23:25:25 +0900

    create useFetch(custom hook)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 import UserComponent from './UserComponent';
+import useFetch from './hooks/useFetch';
 
 function App() {
-  const [users, setUsers] = useState(null);
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setUsers(data);
-    });
-  },[])
+  const users = useFetch("https://jsonplaceholder.typicode.com/users")
+
   return (
     <div className="App">
       <h2>ユーザ一覧</h2>
