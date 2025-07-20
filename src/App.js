03594132963129d59f45259b79186e327f820fec
Your Name <you@example.com>
2025-07-20 11:13:31 +0900

    ちらつき現象防ぐ

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { useLayoutEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ initValue, setInitValue ] = useState("");
+
+  // useEffect(() => {
+  // -> init値を設定あとでレンダー
+  //    ちらつき現象をなくす
+  useLayoutEffect(() => {
+    setInitValue('twonine');
+  }, []);
+
+  console.log("lender!")
+
   return (
     <div className="App">
+      <p>inital is "{initValue}".</p>
+      <p>inital is "{initValue}".</p>
+      <p>inital is "{initValue}".</p>
+      <p>inital is "{initValue}".</p>
+      <p>inital is "{initValue}".</p>
+      <p>inital is "{initValue}".</p>
+      <p>inital is "{initValue}".</p>
     </div>
   );
 }
