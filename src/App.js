0ff86523789bbb8073df2cc36edad7fface0861d
Your Name <you@example.com>
2025-07-20 14:35:09 +0900

    useTransition適用前

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
+let searchResultTest = [
+  { id: 0, name: "なまえ"},
+  { id: 1, name: "なまえ"},
+  { id: 2, name: "なまえ"}
+];
+
 function App() {
   const [timeCount, setTimeCount] = useState(0);
+  const [inputValue, setInputValue] = useState("");
+  const [searchResult, setSearchResult] = useState(null);
+
+  const handleChange = async (e) => {
+    setInputValue(e.target.value);
+
+    setSearchResult(null);
+    const fetchedData = await fetch("https://jsonplaceholder.typicode.com/users").
+      then(async (res) => {
+        return await res.json();
+    });
+    searchResultTest = fetchedData ? fetchedData.map(({id, name}) => { return {id, name}}) : null;
+    setSearchResult(searchResultTest);
+  }
 
   useEffect(() => {
     const timerId = setInterval(() => {
@@ -12,11 +32,22 @@ function App() {
     return () => {
       clearInterval(timerId);
     };
-  }, [])
+  }, []);
 
   return (
     <div className="App">
       <p style={{textAlign:'center'}}>経過：{timeCount}</p>
+
+      <input
+        value={inputValue}
+        onChange={handleChange} />
+      <br/>
+      <label>検索結果</label>
+      <div>
+        {searchResult ? searchResult.map((res, idx) => (
+          <p key={`${idx}_${res.name}`}>{idx},{res.name}</p>
+        )) : null}
+      </div>
     </div>
   );
 }
