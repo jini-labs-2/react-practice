3409c0e5b13d5e1aa86cde8ce07e3dc85aa9a873
Your Name <you@example.com>
2025-07-20 11:53:22 +0900

    経過タイマー、表示

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [timeCount, setTimeCount] = useState(0);
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setTimeCount((prev) => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [])
+
   return (
     <div className="App">
+      <p style={{textAlign:'center'}}>経過：{timeCount}</p>
     </div>
   );
 }
